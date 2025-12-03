fd667cd009b304204266d8dcda468970e5712541
soportecyclops <soportecyclops@gmail.com>
2025-12-03 09:09:57 -0300

    Create _supabaseClient.js

---
+++ api/users/_supabaseClient.js
@@ -0,0 +1,13 @@
+// /api/_supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false
+    }
+  }
+);
